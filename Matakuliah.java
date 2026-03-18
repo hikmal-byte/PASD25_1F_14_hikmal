2c81c94290530e94a27176c0892dd5b3a3fc5384
Hikmal Fadhillah Rasyid Andro Mulyawan <hikmalfadillahrasyid@gmail.com>
2026-03-18 19:06:13 +0700

    Percobaan 3 JB 3 Hikmal
    Percobaan 3 JB 3 Hikmal

---
+++ Matakuliah.java
@@ -0,0 +1,13 @@
+public class Matakuliah {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah (String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
