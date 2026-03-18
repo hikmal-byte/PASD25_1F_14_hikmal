44c780ab01ba6adf30bc8ea984df4152f81f9d16
Hikmal Fadhillah Rasyid Andro Mulyawan <hikmalfadillahrasyid@gmail.com>
2026-03-18 18:47:28 +0700

    Percobaan 1
    Percobaan 1

--- Mahasiswa14.java
+++ Mahasiswa14.java
@@ -1,11 +1,16 @@
-public class Mahasiswa14 {
-    String nama;
-    int nilaiUTS;
-    int nilaiUAS;
 
-    public Mahasiswa14(String nama, int nilaiUTS, int nilaiUAS) {
-        this.nama = nama;
-        this.nilaiUTS = nilaiUTS;
-        this.nilaiUAS = nilaiUAS;
+public class Mahasiswa14{
+    public String nim;
+    public String nama;
+    public String kelas;
+    public float ipk;
+
+    public void cetakInfo() {
+            System.out.println("NIM     : "+ nim);
+            System.out.println("Nama    : "+ nama);
+            System.out.println("Kelas   : "+ kelas);
+            System.out.println("IPK     : "+ ipk);
+            System.out.println("------------------------------------");
     }
 }
+
