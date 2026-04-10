ebec5996d72eebb668ce6aff60f8764cfcfa4751
Hikmal Fadhillah Rasyid Andro Mulyawan <hikmalfadillahrasyid@gmail.com>
2026-04-10 22:38:21 +0700

    Modif Percobaan 2 Minggu 6

--- MahasiswaDemo14.java
+++ MahasiswaDemo14.java
@@ -1,23 +1,36 @@
+
+import java.util.Scanner;
+
 public class MahasiswaDemo14 {
     public static void main(String[] args) {
+        Scanner hikmal14 = new Scanner(System.in);
         MahasiswaBerprestasi14 list = new MahasiswaBerprestasi14();
-        Mahasiswa14 m1 = new Mahasiswa14("123", "Zidan", "2A", 3.2);
-        Mahasiswa14 m2 = new Mahasiswa14("124", "Ayu", "2A", 3.5);
-        Mahasiswa14 m3 = new Mahasiswa14("125", "Sofi", "2A", 3.1);
-        Mahasiswa14 m4 = new Mahasiswa14("126", "Sita", "2A", 3.9);
-        Mahasiswa14 m5 = new Mahasiswa14("127", "Miki", "2A", 3.7);
 
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
+        int jmlMhs = 5;
+        for (int i = 0; i < jmlMhs; i++) {
+            System.out.println("Masukkan data mahasiswa ke-" + (i + 1));
+            System.out.print("NIM   : ");
+            String nim = hikmal14.nextLine();
+            System.out.print("Nama   : ");
+            String nama = hikmal14.nextLine();
+            System.out.print("Kelas   : ");
+            String kelas = hikmal14.nextLine();
+            System.out.print("IPK   : ");
+            double ipk = hikmal14.nextDouble();
+            hikmal14.nextLine();
 
-        System.out.println("Data mahasiswa sebelum sorting: ");
+            Mahasiswa14 m = new Mahasiswa14(nim, nama, kelas, ipk);
+            list.tambah(m);
+            System.out.println("--------------------------------");
+        }
+
+        System.out.println("\nData mahasiswa sebelum sorting: ");
         list.tampil();
 
-        System.out.println("Data mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
+        System.out.println("\nData mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
         list.bubbleSort();
         list.tampil();
+
+        hikmal14.close();
     }
 }
