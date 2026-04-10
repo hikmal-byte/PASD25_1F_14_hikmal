a460868c7ad1f283392ec92608af943d763fc34e
Hikmal Fadhillah Rasyid Andro Mulyawan <hikmalfadillahrasyid@gmail.com>
2026-04-10 22:00:23 +0700

    Percobaan 1 Minggu 6
    Percobaan 1 Minggu 6

---
+++ Images/SortingMain14.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import Minggu6.Sorting14;
+
+public class SortingMain14 {
+    static int a[] = {20, 10, 2, 7, 12};
+    static int b[] = {30, 20, 2, 8, 14};
+    static int c[] = {40, 10, 4, 9, 3};
+
+    static Sorting14 dataurut1 = new Sorting14(a, a.length);
+    static Sorting14 dataurut2 = new Sorting14(b, b.length);
+    static Sorting14 dataurut3 = new Sorting14(c, c.length);
+    public static void main(String[] args) {
+        System.out.println("Data awal 1");
+        dataurut1.tampil();
+        dataurut1.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
+        dataurut1.tampil();
+
+        System.out.println("Data awal 2");
+        dataurut2.tampil();
+        dataurut2.SelectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
+
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
+    }
+}
