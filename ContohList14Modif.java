0223fa96558a214568104e14b8f3a5f7130fadad
Hikmal Fadhillah Rasyid Andro Mulyawan <hikmalfadillahrasyid@gmail.com>
2026-06-26 11:02:43 +0700

    Tugas Selama Final Jobsheet Semester 2: Collection
    Tugas Selama Final Jobsheet Semester 2: Collection

---
+++ ContohList14Modif.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ContohList14Modif {
+    public static void main(String[] args) {
+        List l = new ArrayList<>();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add("4");
+        System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n", l.get(0), l.size(), l.get(l.size() - 1));
+        l.add(4);
+        l.remove(0);
+        System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n", l.get(0), l.size(), l.get(l.size() - 1));
+
+        LinkedList<String> names = new LinkedList<>();
+        names.add("Noureen");
+        names.add("Akhleema");
+        names.add("Shannum");
+        names.add("Uwais");
+        names.add("Al-Qarni");
+
+        names.push("Mei-mei");
+        System.out.printf("Elemen 0: %s total elemen: %d elemen terakhir: %s\n", names.getFirst(), names.size(), names.getLast());
+        System.out.println("Names: " + names.toString());
+    }
+}
